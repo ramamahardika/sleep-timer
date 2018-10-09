d7991b814ca3fda97dc3b71178501e6be0e0c7b2
RamaMahardika <ramamahardika@hotmail.com>
2018-10-09 21:12:58 +0800

    Updating index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const path = require("path");
+const opn = require("opn");
 const app = express();
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 const port = 5000;
 
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'views')));
-app.engine('pug', require('pug').__express)
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "views")));
+app.engine("pug", require("pug").__express);
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
@@ -15,34 +16,43 @@ app.get("/", (req, res) => {
 });
 app.post("/api", (req, res) => {
   const time = req.body.time;
-  if(isNaN(time) || time === "0" ) {
+  if (isNaN(time) || time === "0") {
     return res.status(200).send({
       message: "notnumber"
     });
-  }  
-  exec('shutdown -s -t ' + time, {windowsHide: true}, (err, stdout, stderr) => {
+  }
+  exec(
+    "shutdown -s -t " + time,
+    { windowsHide: true },
+    (err, stdout, stderr) => {
+      if (err) {
+        return res.status(200).send({
+          message: "fail"
+        });
+      }
+      res.status(200).send({
+        message: "success"
+      });
+    }
+  );
+});
+app.post("/cancel", (req, res) => {
+  exec("shutdown -a", { windowsHide: true }, (err, stdout, stderr) => {
     if (err) {
       return res.status(200).send({
         message: "fail"
-      })
+      });
     }
     res.status(200).send({
       message: "success"
-    })
-  });
-});
-app.post("/cancel", (req, res) => {  
-  exec('shutdown -a', {windowsHide: true}, (err, stdout, stderr) => {
-    if (err) {    
-      return res.status(200).send({
-        message: "fail"
-      })
-    }
-    res.status(200).send({
-      message: "success"
-    })
+    });
   });
 });
 app.listen(port, () => {
-  opn('', {app: ['chrome.exe', '--app="http://localhost:'+port +'"]});
-})
+  opn("", {
+    app: ["Chrome.exe", `--app=http://localhost:${port}`],
+    wait: true
+  })
+    .then(done => console.log("Done"))
+    .catch(err => console.log(err));
+});
